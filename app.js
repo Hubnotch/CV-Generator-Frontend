b83e473f52dac3841b69e081174cd7321c2a4c53
kareemah <ajimobikareemah@gmail.com>
2021-06-27 23:42:18 -0700

    add responsiveness to header and hero section

--- app.js
+++ app.js
@@ -1,4 +1,26 @@
+// nav toggle
+const navToggle = document.querySelector(".nav-toggle")
+const nav = document.querySelector(".nav")
+navToggle.addEventListener("click", function() {
+    navToggle.classList.toggle("open")
+    nav.classList.toggle("open")
+})
 
+
+// fixed header
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", function() {
+  if (this.window.scrollY > 100) {
+    header.classList.add("fixed");
+  } else {
+    header.classList.remove("fixed");
+  }
+});
+
+
+
+// slides
 const slides = document.querySelectorAll(".slide");
 const btn = document.querySelector(".next")
     slides.forEach((slide, index)=> {
