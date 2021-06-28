38ba8ceb7c6e97a285198d23bf0470c079d745e1
Hubnotch <love2ekene@gmail.com>
2021-06-28 20:22:34 +0100

    login page

--- app.js
+++ app.js
@@ -1,54 +1,16 @@
-// nav toggle
-const navToggle = document.querySelector(".nav-toggle")
-const nav = document.querySelector(".nav")
-navToggle.addEventListener("click", function() {
-    navToggle.classList.toggle("open")
-    nav.classList.toggle("open")
-})
-
-
-// fixed header
-const header = document.querySelector("header");
-
-window.addEventListener("scroll", function() {
-  if (this.window.scrollY > 100) {
-    header.classList.add("fixed");
+// script for the password toggler
+const password = document.querySelector("#password");
+const passwordToggler = document.querySelector("#togglePassword");
+let toggle = false;
+passwordToggler.addEventListener("click", (e) => {
+  if (toggle === true) {
+    document.querySelector("#togglePassword").src = "images/icons/eye-open.png";
   } else {
-    header.classList.remove("fixed");
+    document.querySelector("#togglePassword").src =
+      "images/icons/eye-closed.png";
   }
+  toggle = !toggle;
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
 });
-
-
-
-// slides
-const slides = document.querySelectorAll(".slide");
-function move() {
-    slides.forEach((slide, index)=> {
-        if(window.innerWidth < 767) {
-            slide.style.left = `${index * 100}%`;
-        }
-        else {
-        slide.style.left = `${index * 50}%`;
-        }
-    })
-
-let counter = 0
-function  carousel() {
-    slides.forEach((slide) => {
-        if (counter === slides.length) {
-            counter = 0;
-          
-        }
-        slide.style.transform = `translateX(-${counter * 100}%)`;
-         
-    })
-    console.log("Hello")
-}
-
-}
-setInterval(move, 1000);
- 
-btn.addEventListener("click", function() {
-    counter++
-    carousel();
-})
