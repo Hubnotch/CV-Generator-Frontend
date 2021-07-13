47cc44350c606f60d6db1d5e3d7a7a1cb4401a29
kareemah <ajimobikareemah@gmail.com>
2021-07-13 02:41:16 -0700

    design the cv-page

--- create-your-cv/cv.js
+++ create-your-cv/cv.js
@@ -6,6 +6,20 @@ const nextBtn = document.querySelector(".next");
 const prevBtn = document.querySelector(".prev");
 
 
+
+const overlay = document.querySelector(".overlay")
+const previewBtn = document.querySelector(".preview-cv");
+
+
+const firstName = document.querySelector("#first-name");
+const lastName = document.querySelector("#last-name");
+const address = document.querySelector("#address");
+const dob = document.querySelector("#dob");
+const profession = document.querySelector("#profession");
+const email = document.querySelector("#email");
+const phoneNumber = document.querySelector("#number");
+
+
 let currentlySelected = 0;
 
 for(let i = 0; i < allIcons.length; i++){
@@ -16,21 +30,21 @@ for(let i = 0; i < allIcons.length; i++){
     allIcons[i].classList.add("active")
         currentlySelected = i;
 
-        if (currentlySelected < 0) {
-            currentlySelected = 0;
-          }
           if (currentlySelected >= allSlides.length - 1) {
-            nextBtn.innerHTML = "Preview CV";
+            nextBtn.style.display = "none";
+            previewBtn.style.display = "block"
           }
           else {
-            nextBtn.innerHTML = `Next<img src=${"../images/next arrow.png"} alt=""`
+            nextBtn.style.display = "inline-flex";
+            previewBtn.style.display = "none"
+
           }
+
           if (currentlySelected > 0) {
             prevBtn.style.display = "block";
           } else {
             prevBtn.style.display = "none";
           }
-
           
     const target = this.getAttribute("data-id");
         for(let j=0; j < allSlides.length; j++) {
@@ -54,14 +68,14 @@ nextBtn.addEventListener("click", function() {
         allIcons[i].classList.remove("active");
     }
 
-    if (currentlySelected < 0) {
-        currentlySelected = 0;
-      }
+   
       if (currentlySelected >= allSlides.length - 1) {
-        nextBtn.innerHTML = "Preview CV";
+        nextBtn.style.display = "none";
+        previewBtn.style.display = "block"
       }
       else {
-        nextBtn.innerHTML = `Next<img src=${"../images/next arrow.png"} alt=""`
+        nextBtn.style.display = "inline-flex";
+        previewBtn.style.display = "none"
       }
       if (currentlySelected > 0) {
         prevBtn.style.display = "block";
@@ -72,7 +86,6 @@ nextBtn.addEventListener("click", function() {
       
     allSlides[currentlySelected].style.display = "block";
     allIcons[currentlySelected].classList.add("active")
-    console.log(currentlySelected)
 })
 
 
@@ -82,7 +95,116 @@ prevBtn.addEventListener("click", function() {
         allSlides[i].style.display = "none";
         allIcons[i].classList.remove("active");
     }
+    if (currentlySelected <= 0) {
+      prevBtn.style.display = "none";
+      nextBtn.style.display = "inline-flex";
+      previewBtn.style.display = "none";
+    }
+    else {
+      // prevBtn.style.display = "inline-flex"
+    }
+    if (currentlySelected <= allSlides.length - 1) {
+      nextBtn.style.display = "inline-flex";
+      previewBtn.style.display = "none"
+    }
     allSlides[currentlySelected].style.display = "block";
     allIcons[currentlySelected].classList.add("active")
-    console.log(currentlySelected)
 })
+
+
+
+previewBtn.addEventListener("click", function() {
+  overlay.style.display = "flex";
+  overlay.innerHTML = `   <div class="fixed-cv-header">
+  <button class="close-preview">Close Preview</button>
+</div>
+<div class="cv-page">
+  <div class="cv-header">
+    <div class="inner-rect">
+      <h1>${(firstName.value.toUpperCase())} ${(lastName.value).toUpperCase()}</h1>
+      <p>${(profession.value.toUpperCase())}</p>
+      <img src="../images/samantha.png" alt="">
+    </div>
+  </div>
+
+  <!-- cv body -->
+
+  <!-- profile -->
+  <div class="profile">
+      <div class="col-33">
+        <h3>Profile</h3>
+      </div>
+      <div class="col-33">
+       <p> <b>Date of Birth: </b>${dob.value}</p>
+       <p><b> Phone: ${phoneNumber.value} </b></p>
+
+      </div>
+      <div class="col-33">
+       <p><b> Mail: </b>${email.value}</p>
+       <p><b>Address: </b> ${address.value} </p>
+
+      </div>
+  </div>
+
+
+  <!-- education -->
+  <div class="education">
+    <h3>Education</h3>
+    <div class="education-row">
+      <div class="col-33">Nov 2005 — Sep 2010</div>
+    <div class="col-66">
+      <p><b>Los Angelis University</b></p>
+      <p>Bachelor of Fine Arts in Graphic Design, GPA: 3.4/4.0</p>
+    </div>
+    <div class="col-33">Nov 2005 — Sep 2010</div>
+    <div class="col-66">
+      <p><b>Los Angelis University</b></p>
+      <p>Bachelor of Fine Arts in Graphic Design, GPA: 3.4/4.0</p>
+    </div>
+    </div>
+  </div>
+
+
+  
+  <!-- employement -->
+  <div class="employement">
+    <h3>Employement</h3>
+    <div class="employement-row">
+      <div class="col-33">Nov 2005 — Sep 2010</div>
+    <div class="col-66">
+      <p><b>Los Angelis University</b></p>
+      <p>Bachelor of Fine Arts in Graphic Design, GPA: 3.4/4.0</p>
+    </div>
+    </div> 
+  </div>
+
+    <!-- skills -->
+    <div class="skills">
+      <div class="skills-row">
+        <div class="col-33">Nov 2005 — Sep 2010</div>
+      <div class="col-66">
+        <p><b>Los Angelis University</b></p>
+        <p>Bachelor of Fine Arts in Graphic Design, GPA: 3.4/4.0</p>
+      </div>
+      </div> 
+    </div>
+
+    <div class="bottom-buttons">
+      <button type="button" class="download-preview"><img src="../images//cloud-download-alt (1).png" alt="">Downlaod</button>
+      <button type="button" class="close-preview-white">Close Preview</button>
+    </div>
+  </div>`
+  console.log(lastName, firstName);
+
+  const closeBtn = document.querySelector(".close-preview");
+const closeBtn2 = document.querySelector(".close-preview-white")
+closeBtn.addEventListener("click", function() {
+  overlay.style.display = "none";
+})
+closeBtn2.addEventListener("click", function() {
+  overlay.style.display = "none";
+})
+})
+
+
+// close preview button
